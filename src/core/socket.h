41e2de067760a43b9da577958421ac8703ca0313
NA29 <nathan.audegond@hotmail.com>
2026-01-23 22:12:30 -0500

    finished socket.h

--- src/core/socket.h
+++ src/core/socket.h
@@ -1,17 +1,26 @@
 class Socket {
 private:
-  int fd;
+  int fd_;
 
 public:
   Socket();
+
   // otherwise any int can become a Socket implicitly (UB)
   // ex: handleTask(Sockets) will not be flagged by compiler
   explicit Socket(int fd);
+
   ~Socket();
 
   // important for RAII!!, prevents two objects from accessing the same resource
-  // copy operator : creating a socker from another one
+  // copy constructor : creating a socker from another one (ref)
   Socket(const Socket &) = delete;
+
   // copy assignment operator : assigning a socker into an existing socket
   Socket &operator=(const Socket &) = delete;
+
+  // move constructor
+  Socket(Socket &&) noexcept;
+
+  // move assignment operator
+  Socket &operator=(Socket &&) noexcept;
 };
